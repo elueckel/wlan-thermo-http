c406c4ff4a08be58e58d7627ec5d33825d4fb0c9
elueckel <enno@lueckel.net>
2021-07-26 12:11:16 +0200

    .

--- wlan-thermo-http/module.php
+++ wlan-thermo-http/module.php
@@ -104,15 +104,15 @@ if (!defined('vtBoolean')) {
 			$vpos = 10 * ceil($vpos/10);
 
 			$Channel_LowerTargetID= @IPS_GetObjectIDByIdent('Channel'.$Channel.'_LowerTarget', $this->InstanceID);	
-			$this->EnableAction($Channel_LowerTargetID);
 			if (IPS_GetObject($Channel_LowerTargetID)['ObjectType'] == 2) {
 					$this->RegisterMessage($Channel_LowerTargetID, VM_UPDATE);
+					$this->EnableAction($Channel_LowerTargetID);
 			}
 
 			$Channel_HigherTargetID= @IPS_GetObjectIDByIdent('Channel'.$Channel.'_HigherTarget', $this->InstanceID);
-			$this->EnableAction($Channel_HigherTargetID);	
 			if (IPS_GetObject($Channel_HigherTargetID)['ObjectType'] == 2) {
 					$this->RegisterMessage($Channel_HigherTargetID, VM_UPDATE);
+					$this->EnableAction($Channel_HigherTargetID);	
 			}
 
 			$ChannelActive = $this->ReadPropertyBoolean("Channel".$Channel."Active");
